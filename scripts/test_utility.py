aa8d60a1505789c9584604953270cf6ee89b6dd9
Arthur Albarelli <arthuralbarelli@gmail.com>
2021-12-09 14:25:12 -0300

    Create unit tests

---
+++ scripts/test_utility.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import pytest
+
+from utility import load_data
+
+
+def test_load_data():
+    X, y, cols = load_data("data/Placement_Data_Full_Class.csv")
+
+    # Type check
+    assert isinstance(X, np.ndarray)
+    assert isinstance(y, pd.core.series.Series)
+    assert isinstance(cols, list)
+
+    # Shape check
+    assert X.shape[0] == y.shape[0]
+    assert X.shape[1] >= 1
